ae92243de1522ee47fb2b775d9110ece643a0e6d
Gustavo Andreli <gu_andreli@hotmail.com>
2018-01-09 11:42:53 -0200

    inclusão classe Posição

--- xadrexConsole/Program.cs
+++ xadrexConsole/Program.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tabuleiro;
 
 namespace xadrexConsole {
     class Program {
         static void Main(string[] args) {
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine(p);
+            Console.ReadKey();
         }
     }
 }
